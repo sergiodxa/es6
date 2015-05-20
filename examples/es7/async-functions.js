0bab0bd5cb1a9566b2a12d2f71fdab7a4c95f683
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-20 03:10:23 -0300

    mejorado ejemplo de async/await

--- examples/es7/async-functions.js
+++ examples/es7/async-functions.js
@@ -1,12 +1,14 @@
-async function exclamatify () {
-  try {
-    const datos = await getMessage(); // hola mundo
-    return `${datos}!`;
-  } catch (error) {
-    console.log(error);
-  }
-}
+const app = Object.freeze({
+  init: async function init () {
+    try {
+      const data1 = await getData(); // hola
+      const data2 = `${data1} mundo`;
 
-(async function () {
-  console.log(await exclamatify()); // hola mundo!
-})();
+      console.log(data2);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+});
+
+app.init();
